c4a915a118534099e2f161cf001d68e592c8ce61
Anastasiia Maidaniuk <maidanyuk.2004@gmail.com>
2025-07-18 15:56:39 +0300

    update

--- src/app.js
+++ src/app.js
@@ -3,12 +3,34 @@
 
 const fs = require('node:fs');
 
-const [sourcePath, destinationPath] = process.argv.slice(2);
+function copyFile(sourcePath, destinationPath) {
+  if (!sourcePath || !destinationPath) {
+    throw new Error('Please provide both source and destination file paths.');
+  }
 
-try {
+  if (sourcePath === destinationPath) {
+    return;
+  }
+
   const source = fs.readFileSync(sourcePath).toString();
 
   fs.writeFileSync(destinationPath, source);
-} catch (error) {
-  console.error(`Error: ${error.message}`);
 }
+
+if (require.main === module) {
+  const [sourcePath, destinationPath] = process.argv.slice(2);
+
+  try {
+    copyFile(sourcePath, destinationPath);
+  } catch (error) {
+    if (error.code === 'EISDIR') {
+      console.error(error.message);
+    } else if (error.code === 'ENOENT') {
+      console.error(error.message);
+    } else {
+      console.error(error.message);
+    }
+  }
+}
+
+module.exports = copyFile;
